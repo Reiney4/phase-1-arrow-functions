bb203ca2e2d3d3ae77b6584a57c309fb9d5499d8
Valerie <valeriejeptoo4@gmail.com>
2023-06-17 15:58:53 +0300

    completed

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function (param1 =  2000,param2 = 
+    100) {
+  return (20) ; 
+}
+let x = 2
+const square = parameter1 =x => 
+   { return (4)}
+
+let a = 3
+let b = 4
+const add = (param1=a, param2=b)=> {
+    return (a+b);
+}
